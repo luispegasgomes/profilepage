3b2450ce0f6678e86e07bc0220d14a6737af1aba
Luís Gomes <luispegasgomes@gmail.com>
2023-08-11 00:51:58 +0100

    #4 - Testing modal size

--- src/components/modals/modal-projects.vue
+++ src/components/modals/modal-projects.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="modalContentClass">
-        <b-modal v-model="show" centered @hidden="onModalHidden" hide-header hide-footer body-bg-variant="dark">
+        <b-modal v-model="show" centered @hidden="onModalHidden" hide-header hide-footer body-bg-variant="dark" size="xl">
             <div class="d-flex flex-column align-items-center justify-content-between">
                 <h1 class="mb-4 fontAsap colorBlue" style="font-size:30px">{{ projectInfo.name }}</h1>
-                <div class="fontBarlow" style="font-size: 30px">
-                    <img :src="projectInfo.promo" width="300px" />
+                <div class="d-flex justify-content-center">
+                    <img :src="projectInfo.promo" width="95%"/>
                 </div>
-                <img src="../../assets/icons/close.png" width="10%" @click="hideModal" style="cursor: pointer;" class="mt-3">
+                <img src="../../assets/icons/close.png" width="50px" @click="hideModal" style="cursor: pointer;" class="mt-3">
             </div>
         </b-modal>
     </div>
